7bcd29eeebf87145ed60c92f8408172782dee7c3
weidache <johannes.weidacher@rohde-schwarz.com>
2016-09-26 13:53:36 +0200

    use correct brackets and spelling

--- PowerCLI/initialize.ps1
+++ PowerCLI/initialize.ps1
@@ -1,7 +1,8 @@
-#### PowerCLI Initialization. Put this in fron of all Powershell scripts to use PowerCLI CMDlets
+#### PowerCLI Initialization. Put this in front of all Powershell scripts to use PowerCLI CMDlets
 ### Taken from blogs.vmware.com
 
 
-if ( !(Get-Module -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue) ) {
-. “C:\Program Files (x86)\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1″
+if ( !(Get-Module -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue) ) 
+{
+    "C:\Program Files (x86)\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
 }
